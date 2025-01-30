32932aa437a2086578135eb869c98160542d4199
Sangram Thapa <thapasangram64@gmail.com>
2025-01-30 13:17:09 +0545

    apifetch

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,6 @@ function Home() {
     try {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
-      console.log(data);
       setProduct(data);
     } catch (error) {
       console.error(error);
@@ -14,23 +13,36 @@ function Home() {
   }
 
   return (
-    <>
-      <div>
-        <button onClick={handleAllProduct}>Load Products</button>
-        <ul className="grid grid-cols-3 gap-40 px-50 justify-center item-center">
-          {product.map((item) => (
-            <li key={item.id}>
-              <div>
-                <div>
-                  <img src={item.image} className="w-40 border-2 p-10 " />
-                </div>
-                <div>Price: {item.price}</div>
-              </div>
-            </li>
-          ))}
-        </ul>
+    <div>
+      <button
+        onClick={handleAllProduct}
+        className="bg-red-600 hover:bg-red-500 text-white px-6 py-2 rounded-md mb-8 "
+      >
+        Load Products
+      </button>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16  ">
+        {product.map((item) => (
+          <div
+            key={item.id}
+            className=" p-10 rounded shadow-md hover:shadow-gray-600"
+          >
+            <div>
+              <img
+                src={item.image}
+                alt={item.title}
+                className=" w-48 h-48 flex mb-2"
+              />
+            </div>
+            <h2 className="font-semibold">{item.title}</h2>
+            <p className="text-green-400">
+              $<spam className="text-black">{item.price}</spam>
+            </p>
+            <p>{item.description}</p>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
