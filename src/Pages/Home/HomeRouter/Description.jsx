7a8d990729c5a4b5fc72a2afbf9b03dcbd5d7baf
Sangram Thapa <thapasangram64@gmail.com>
2025-01-31 13:27:49 +0545

    added

--- src/Pages/Home/HomeRouter/Description.jsx
+++ src/Pages/Home/HomeRouter/Description.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Description() {
   const params = useParams();
+  const [state, setState] = useState(" ");
+  async function singleFetch() {
+    try {
+      const response = await fetch(
+        "https://fakestoreapi.com/products/" + params.desc
+      );
+      const data = await response.json();
+      console.log(data);
+      setState(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    singleFetch();
+  }, []);
+
   if (isNaN(params.desc)) {
     alert("it is not a numner");
   }
 
-  return <div>Description ...</div>;
+  return (
+    <div>
+      <div>{state && <img src={state.image}></img>}</div>
+    </div>
+  );
 }
 
 export default Description;
