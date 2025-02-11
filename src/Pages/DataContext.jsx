3aed0a803677e8b736e53476b466bc6e7220f2e5
Sangram Thapa <thapasangram64@gmail.com>
2025-02-11 12:03:33 +0545

    added'

--- src/Pages/DataContext.jsx
+++ src/Pages/DataContext.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const newContext = createContext();
 
 export function DataContext({ children }) {
   const [state, setState] = useState();
+
+  async function fetch() {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      console.log(data);
+      setState(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetch();
+  }, []);
   return (
     <newContext.Provider value={{ state, setState }}>
       {children}
